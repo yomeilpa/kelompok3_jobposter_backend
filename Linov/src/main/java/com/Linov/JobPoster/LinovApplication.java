65bc051cff6db1d76e2b612a7c31a686ac8759ca
Afdhalul Ihsan <linovhr.jobposter@gmail.com>
2020-01-17 11:20:01 +0700

    Update Tomcat

--- Linov/src/main/java/com/Linov/JobPoster/LinovApplication.java
+++ Linov/src/main/java/com/Linov/JobPoster/LinovApplication.java
@@ -3,15 +3,12 @@ package com.Linov.JobPoster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 
 @SpringBootApplication
-public class LinovApplication {
-
-	
-
-
+public class LinovApplication  extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(LinovApplication.class, args);
 
