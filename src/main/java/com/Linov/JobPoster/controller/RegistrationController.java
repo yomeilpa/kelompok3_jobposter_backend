30ed19b7b4b213f0138e40164a7a310898deb9ab
Afdhalul Ihsan <linovhr.jobposter@gmail.com>
2020-01-22 11:23:52 +0700

    hahah

--- src/main/java/com/Linov/JobPoster/controller/RegistrationController.java
+++ src/main/java/com/Linov/JobPoster/controller/RegistrationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -86,7 +87,7 @@ public class RegistrationController {
 		
 	
 	@PutMapping("/uploadphoto/{id}")
-	public ResponseEntity<?> uploadiProfile(@RequestPart MultipartFile[] upload,CandidateModel candidate,@PathVariable("id") String id){
+	public ResponseEntity<?> uploadiProfile(@RequestParam MultipartFile[] upload,CandidateModel candidate,@PathVariable("id") String id){
 		candidate = this.candidate.findById(id);
 		try {
 			candidate.setFilename(upload[0].getOriginalFilename());
