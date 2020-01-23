c83be663917b84e52f3e5547529998f347f784c7
Afdhalul Ihsan <linovhr.jobposter@gmail.com>
2020-01-23 15:00:16 +0700

    Akksks

--- src/main/java/com/Linov/JobPoster/controller/InterviewController.java
+++ src/main/java/com/Linov/JobPoster/controller/InterviewController.java
@@ -3,6 +3,7 @@ package com.Linov.JobPoster.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.Linov.JobPoster.service.InterviewStatusService;
 
 @RestController
 @Controller
+@CrossOrigin("*")
 public class InterviewController {
 
 	@Autowired
--- src/main/java/com/Linov/JobPoster/controller/SkillLevelController.java
+++ src/main/java/com/Linov/JobPoster/controller/SkillLevelController.java
@@ -3,6 +3,7 @@ package com.Linov.JobPoster.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.Linov.JobPoster.service.SkillLevelService;
 
 @RestController
 @Controller
+@CrossOrigin("*")
 public class SkillLevelController {
 
 	@Autowired
