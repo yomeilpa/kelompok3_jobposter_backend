25c8ab141db3286a6c54c7b1b2667afa1be7b8b4
Afdhalul Ihsan <linovhr.jobposter@gmail.com>
2020-01-24 13:47:05 +0700

    Yo Whats App

--- src/main/java/com/Linov/JobPoster/controller/SkillLevelController.java
+++ src/main/java/com/Linov/JobPoster/controller/SkillLevelController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,5 +42,10 @@ public class SkillLevelController {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 		
+	}
+	
+	@GetMapping("/skill")
+	public ResponseEntity<?> findByid(){
+		return ResponseEntity.ok(cands.findAll());
 	}
 }
