e93123e70e56fd3c9c7e11739790978f547bbca9
Afdhalul Ihsan <linovhr.jobposter@gmail.com>
2020-01-31 15:17:44 +0700

    Repair 20

--- src/main/java/com/Linov/JobPoster/dao/JobPostingDao.java
+++ src/main/java/com/Linov/JobPoster/dao/JobPostingDao.java
@@ -53,10 +53,10 @@ public class JobPostingDao  extends CommonDao{
 	public List<JobPostingModel> finByFilter(FilterJobPosting eg) {
 		StringBuilder query = new StringBuilder();
 		query.append("FROM JobPostingModel jp where 1=1");
-		if(eg.getProvince() != null) {
+		if(eg.getProvince().getProvince() != null) {
 			query.append(" and lower(jp.city.province.province) like:f1");
 		}
-		if(eg.getCity() != null) {
+		if(eg.getCity().getCity() != null) {
 			query.append(" and lower(jp.city.city) like:f2");
 		}
 		if(eg.getTitle() != null) {
@@ -70,10 +70,10 @@ public class JobPostingDao  extends CommonDao{
 		}
 		
 		Query exc = super.entityManager.createQuery(query.toString());
-		if(eg.getProvince() != null) {
+		if(eg.getProvince().getProvince() != null) {
 			exc.setParameter("f1", (eg.getProvince().getProvince().toLowerCase()));
 		}
-		if(eg.getCity() != null) {
+		if(eg.getCity().getCity() != null) {
 			exc.setParameter("f2", eg.getCity().getCity().toLowerCase());
 		}
 		if(eg.getTitle() != null) {
