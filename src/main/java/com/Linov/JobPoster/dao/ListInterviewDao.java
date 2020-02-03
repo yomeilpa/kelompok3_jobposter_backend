599c679aeaaa766abebd9003d7a7fd9d4988ba15
Afdhalul Ihsan <linovhr.jobposter@gmail.com>
2020-02-03 16:07:06 +0700

    Rpeairing Query

--- src/main/java/com/Linov/JobPoster/dao/ListInterviewDao.java
+++ src/main/java/com/Linov/JobPoster/dao/ListInterviewDao.java
@@ -60,7 +60,7 @@ public class ListInterviewDao extends CommonDao{
 	@SuppressWarnings("unchecked")
 	public List<ListofInterviewModel> findAllbyPoster(String id) {
 		List<ListofInterviewModel> lstCandidateModels = super.entityManager
-				.createQuery("" + "From ListofInterviewModel ba where ba.job.job.candidate.id=:id").setParameter("id", id).getResultList();
+				.createQuery("" + "From ListofInterviewModel ba where ba.job.job.candidate.id=:id and status.name != 'WILL ATTEND'").setParameter("id", id).getResultList();
 		if (lstCandidateModels.size() == 0) {
 			return lstCandidateModels;
 		} else
