c0ee15e7e2fce34a7106be16956e7b087341f51a
Afdhalul Ihsan <linovhr.jobposter@gmail.com>
2020-02-07 11:28:42 +0700

    Configure

--- src/main/java/com/Linov/JobPoster/config/WebSecurityConfig.java
+++ src/main/java/com/Linov/JobPoster/config/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package com.Linov.JobPoster.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -55,13 +57,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
 
     }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    	web.ignoring().antMatchers(HttpMethod.OPTIONS);
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.csrf().disable()
 
-                .authorizeRequests().antMatchers("/**").permitAll().
+                .authorizeRequests().antMatchers("/register/**","/jobposting/**","/province/**","/prov/city/**","/jwt/login").permitAll().
 
         anyRequest().authenticated().and().
 
